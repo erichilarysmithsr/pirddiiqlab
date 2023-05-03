859888c6c1d5c2c16f07ae930fc522a6f232516c
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-05-03 15:19:45 -0500

    Create MealPrepCCOLife1.py

---
+++ MealPrepCCOLife1.py
@@ -0,0 +1,15 @@
+# Original code
+meal_code = "0500000000"
+meal_description = "Beans and toast"
+meal_category = "Meal"
+meal_type = "Breakfast"
+meal_frequency = "Daily"
+meal_provider = "Warren Barr Gold Coast"
+
+# Refactored code
+meal_code = "0500000000"
+meal_description = "Beans and toast"
+meal_category = "Meal"
+meal_type = "Breakfast"
+meal_frequency = "Daily"
+meal_provider = "Cornerstone Community Outreach in Uptown"
