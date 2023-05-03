3438ec32fbbf9e92bbc31da478094670d3a1d00c
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-05-03 15:24:55 -0500

    Create MealPrepPacificGardenMissionsShelter.py

---
+++ MealPrepPacificGardenMissionsShelter.py
@@ -0,0 +1,41 @@
+# Import the necessary libraries
+import numpy as np
+import pandas as pd
+
+# Define the constants
+RDA_PROTEIN = 60
+SERVING_SIZE_BEANS = 1
+SERVING_SIZE_BREAD = 2
+PROTEIN_PER_SERVING_BEANS = 5
+CARBOHYDRATE_PER_SERVING_BEANS = 21
+PROTEIN_PER_SERVING_BREAD = 2
+CARBOHYDRATE_PER_SERVING_BREAD = 11
+
+# Calculate the number of servings of beans and bread needed
+NUM_SERVINGS_BEANS = RDA_PROTEIN / PROTEIN_PER_SERVING_BEANS / 2
+NUM_SERVINGS_BREAD = (139 - CARBOHYDRATE_PER_SERVING_BEANS * NUM_SERVINGS_BEANS) / CARBOHYDRATE_PER_SERVING_BREAD
+
+# Prepare the beans
+# - Open a can of Campbell's pork beans
+# - Heat the beans in a pot on the stove
+# - Serve the beans
+
+# Prepare the toast
+# - Toast two slices of white bread
+# - Serve the toast
+
+# Code the meal in Python for ICD 11
+# - The meal code is "0500000000"
+# - The meal description is "Beans and toast"
+# - The meal category is "Meal"
+# - The meal type is "Breakfast"
+# - The meal frequency is "Daily"
+# - The meal provider is "Pacific Gardens Mission Shelter for SNAP Office at 1112 South Wabash in Chicago"
+
+# Print the meal information
+print("Meal code: 0500000000")
+print("Meal description: Beans and toast")
+print("Meal category: Meal")
+print("Meal type: Breakfast")
+print("Meal frequency: Daily")
+print("Meal provider: Pacific Gardens Mission Shelter for SNAP Office at 1112 South Wabash in Chicago")
